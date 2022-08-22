88478ff433c29ab10f83751e0c0e8b00eb893dd0
daheeamond <daheeyeu@gmail.com>
2022-08-22 17:13:47 +0900

    Add controller

---
+++ android/app/src/main/java/com/example/webview_prac/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.webview_prac;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Screen/Home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  WebViewController? controller;
+  final homeurl='https://velog.io/@steadygo247/Flutterinappwebview';
+
+ HomeScreen({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+         backgroundColor: Colors.amber,
+         title: Text('Lia blog'),
+         centerTitle: true,
+         actions: [
+           IconButton(onPressed: (){
+             if(controller == null) {
+               return;
+             }
+             controller!.loadUrl(homeurl);
+           },
+             icon: Icon(
+               Icons.home
+             ),
+           ),
+         ],
+       ),
+        body: SafeArea(
+        child: WebView(
+          onWebViewCreated:(WebViewController controller){
+            this.controller = controller;
+          } ,
+          initialUrl: homeurl,
+          javascriptMode: JavascriptMode.unrestricted,
+          gestureNavigationEnabled: true,
+        )
+        )
+    );
+  }
+}
